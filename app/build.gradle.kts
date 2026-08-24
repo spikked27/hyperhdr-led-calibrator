7b690bdedab0968fea1a24c7dfe37350e321e66c
spikked27 <mattterracciano@gmail.com>
2026-08-23 20:26:14 -0400

    Beta 9.1: bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.spikked27.hyperhdrcalibrator"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.1.0-beta.9"
+        versionCode = 10
+        versionName = "0.1.0-beta.9.1"
         testInstrumentationRunner = "android.app.InstrumentationTestRunner"
     }
 
